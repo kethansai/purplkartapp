5b85deec386deb0d2057837a1d96bd580b8be3cd
kethansai <kethansai77@gmail.com>
2022-01-24 02:07:28 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className='bg-slate-900 text-white'>
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
