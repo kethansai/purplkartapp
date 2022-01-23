d892ba7d4664295265fc495b16f15fccf0bf3572
kethansai <kethansai77@gmail.com>
2022-01-24 05:22:11 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className='bg-slate-900 text-white'>
+    <div className='bg-slate-900 text-white h-full'>
       <Nav/>
     </div>
   );
